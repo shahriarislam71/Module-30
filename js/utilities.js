7c44485a39ec7daae15b8079cd50ff15f862a54d
shahriarislam <shahriarislamrafi71@gmail.com>
2023-12-07 19:16:47 +0600

    fifth commit

--- js/utilities.js
+++ js/utilities.js
@@ -6,4 +6,34 @@ function randomBackground(eventt){
     let bgColor = `rgb(${x}, ${y}, ${z})`
     eventt.target.style.background = bgColor;
   }
+
+// function for finding input field value
+function InputFieldValue(Id){
+  const inputId = document.getElementById(Id)
+  return parseFloat(inputId.value)
+}
+
+// function for calculating area
+function CalculateArea(Id,value1, value2){
+  const propertyName = document.getElementById(Id)
+  if (propertyName.innerText === 'Triangle'){
+    return 0.5*value1*value2
+  }
+  else if (propertyName.innerText == 'Rectangle'){
+    return value1*value2
+  }
+  else if (propertyName.innerText === 'Parallelogram'){
+    return value1*value2
+  }
+  else if (propertyName.innerText === 'Rhombus'){
+    return 0.5*value1*value2
+  }
+  else if(propertyName.innerText === 'Pentagon'){
+    return 0.5*value1*value2
+  }
+  else{
+    return 3.14*value1*value2
+  }
+
+}
   
